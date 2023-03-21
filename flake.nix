1390781532be05b94087a7af9f0969963c5a038e
Martin Becze <martin@becze.org>
2023-03-21 06:31:22 +0100

    moved frontend to its own folder

--- flake.nix
+++ flake.nix
@@ -1,24 +1,50 @@
 {
-  description = "Your next node.js project";
+  description = "UAP - Unstoppable Affiliates Protocol";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    foundry.url = "github:shazow/foundry.nix/monthly"; # Use monthly branch for permanent releases
+    muKnIOpkgs.url = "github:MuKnIO/nixpkgs/solc"; # solc in master is broken and out of date
+  };
+
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
+    utils,
+    foundry,
+    muKnIOpkgs,
   }:
-    flake-utils.lib.eachDefaultSystem
-    (system: let
+    utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
-      };
-    in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nodejs_latest
-          nodePackages.pnpm
-          nodePackages.typescript
-          nodePackages.typescript-language-server
-          nodePackages.prettier
+        overlays = [
+          (self: super: {
+            solc = muKnIOpkgs.legacyPackages.${system}.solc;
+          })
+          foundry.overlay
         ];
       };
+    in {
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [
+            # smart contracct dependencies
+            foundry-bin
+            solc
+
+            # frontend dependencies
+            nodejs_latest
+            nodePackages.pnpm
+            nodePackages.typescript
+            nodePackages.prettier
+            # for editor
+            nodePackages.typescript-language-server
+          ];
+
+          # Decorative prompt override so we know when we're in a dev shell
+          shellHook = ''
+            export PS1="[dev] $PS1"
+          '';
+        };
     });
 }
