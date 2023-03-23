67fc5cbe1d64f5251fbd6b761f2e604a29808cbc
Martin Becze <martin@becze.org>
2023-03-23 20:43:25 +0100

    display address

--- frontend/src/app/[address]/page.tsx
+++ frontend/src/app/[address]/page.tsx
@@ -5,7 +5,8 @@ import Image from "next/image";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Page({ params }: { params: { address: string } }) {
-  const rootHash =  "bafyreigi6k3a3ofqqdrsuyrzer5kn3kv5q72x5xmoyuyddomdwtkgoxf3e"
+  const rootHash =
+    "bafyreigi6k3a3ofqqdrsuyrzer5kn3kv5q72x5xmoyuyddomdwtkgoxf3e";
 
   const { data, error, isLoading } = useSWR(
     `http://${rootHash}.ipfs.localhost:8080/?format=dag-json`,
@@ -17,20 +18,23 @@ export default function Page({ params }: { params: { address: string } }) {
 
   // render data
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {data.map((item, index) => {
-        return (
-          <div>
-            <Image
-              src={`http://${rootHash}.ipfs.localhost:8080/${index}/picture`}
-              alt={item.description}
-              width={500}
-              height={500}
-              unoptimized
-            />
-          </div>
-        );
-      })}
+    <div>
+      <h1>{params.address}</h1>
+      <div className="grid grid-cols-4 gap-4">
+        {data.map((item, index) => {
+          return (
+            <div>
+              <Image
+                src={`http://${rootHash}.ipfs.localhost:8080/${index}/picture`}
+                alt={item.description}
+                width={500}
+                height={500}
+                unoptimized
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
   // return <h1>Hello, {params.address}</h1>;
