1cd204609ef795c0b13580cf18c6ba3d8542a973
Martin Becze <martin@becze.org>
2023-04-23 21:30:15 +0200

    get the data from form

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,50 +1,9 @@
 "use client";
 
-import React from "react";
-import { useRouter } from "next/navigation";
-import { usePrepareContractWrite, useContractWrite, useAccount } from "wagmi";
-import storeABI from "../../../contracts/out/store-reg.sol/Store.json" assert { type: "json" };
-import depolyerTx from "../../../contracts/broadcast/store-reg.s.sol/31337/run-latest.json" assert { type: "json" };
+import Link from "next/link";
 
 export default function Page() {
-  const router = useRouter();
-  const { address, isConnecting, isDisconnected } = useAccount();
-
-  const {
-    config,
-    error: prepareError,
-    isError: isPrepareError,
-  } = usePrepareContractWrite({
-    address: depolyerTx.transactions[0].contractAddress,
-    abi: storeABI.abi,
-    functionName: "mintTo",
-    args: [address],
-    enabled: true,
-  });
-
-  const { data, isLoading, isSuccess, write } = useContractWrite(config);
-  if (isSuccess) {
-    console.log(data);
-    router.push(`/${data}/edit`);
-  }
-  return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          write?.();
-        }}
-      >
-        <button disabled={!write || isLoading}>
-          {isDisconnected
-            ? "connect to Mint"
-            : isLoading
-            ? "Minting..."
-            : "Mint"}
-        </button>
-        {isSuccess && <div>Successfully minted your Store!</div>}
-        {isPrepareError && <div>Error: {prepareError?.message}</div>}
-      </form>
-    </div>
-  );
+  return <div>
+    <Link href="/create">Mint a Store!</Link>
+  </div>;
 }
